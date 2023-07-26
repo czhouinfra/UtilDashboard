f11717b77df81dd7395f80e5c368683e23528e1c
czhouinfra <140426560+czhouinfra@users.noreply.github.com>
2023-07-26 15:28:54 +0800

    Update utildashboard.py

--- utildashboard.py
+++ utildashboard.py
@@ -1,33 +1,35 @@
 import dash
+import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import pandas_datareader.data as pdr
 from datetime import datetime
-import yfinance as yf
 import dash_auth
-
-yf.pdr_override()
 
 USERNAME_PASSWORD_PAIRS = [['username','password'],['gcinfra','gcinfra']]
 
+df = pd.read_excel('C:/Users/congyun_zhou/PycharmProjects/UtilDashboard/Dashboard.xlsm', 'Contract')
+contract_buyer = df['Column1.buyer'].unique()
+contract_sign_date = df['Column1.original_signing']
+contract_volume = df['Column1.contract_volume_mmt']
+
+
 app = dash.Dash()
-server = app.server
 
 auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
 
 app.layout = html.Div([
-                html.H1('New Stock Chart'),
+                html.H1('China (Mainland) LNG Contract Dashboard'),
                 html.Div([
-                    html.H3('Enter a new stock symbol',style={'paddingRight':'30px'}),
-                    dcc.Input(id='stock_ticker_input',
-                                value='AAPL',
-                              style={'fontSize':24, 'width': 75}),
-                    ], style={'display': 'inline-block','verticalAlign':'top'}),
+                    html.H3('Select Buyer',style={'paddingRight':'30px'}),
+                    dcc.Dropdown(contract_buyer, id='contract_buyer_dropdown',
+                              style={'fontSize':18, 'width': 400}),
+                    ], style={'display': 'inline-block','verticalAlign':'top','paddingRight':'30px'}),
                 html.Div([
                     html.H3('Select a start date and end date:'),
                     dcc.DatePickerRange(id='date_picker_input',
-                                        min_date_allowed=datetime(2019,1,1),
+                                        min_date_allowed=datetime(2005,1,1),
                                         max_date_allowed=datetime.today(),
                                         start_date=datetime(2020,1,1),
                                         end_date=datetime.today()
@@ -38,7 +40,7 @@ app.layout = html.Div([
                             children='Submit',
                             style={'fontSize':24, 'marginLeft':'30px'}),
                 dcc.Graph(
-                    id='stock_chart',
+                    id='chart1',
                     figure={
                         'data': [],
                         'layout': {'title': 'name'}
@@ -46,28 +48,30 @@ app.layout = html.Div([
                 )
 ])
 
-@app.callback(Output('stock_chart','figure'),
+@app.callback(Output('chart1','figure'),
               [Input('submit_button','n_clicks')],
-              [State('stock_ticker_input','value'),
+              [State('contract_buyer_dropdown','value'),
                State('date_picker_input','start_date'),
                State('date_picker_input','end_date')],
               )
 
-def generate_output(n_clicks, stock_ticker, start_date, end_date):
-    start = start_date
-    end = end_date
-    df = pdr.get_data_yahoo(stock_ticker, start, end)
+def generate_contract_buyer(n_clicks, contract_buyer, start_date, end_date):
 
+    selected_df = df[(df['Column1.buyer']==contract_buyer)
+                    &(df['Column1.original_signing']>=start_date)
+                    &(df['Column1.original_signing']<=end_date)]
     fig = {
         'data': [
-            {'x': df.index,
-            'y': df['Close']}
+            go.Bar(x=selected_df['Column1.original_signing'],
+                   y=selected_df['Column1.contract_volume_mmt'])
         ],
-        'layout': {'title': stock_ticker}
+        'layout': {'title': 'Buyer and Signed Date',
+                   'xaxis': {'range': [start_date, end_date]}}
     }
     return  fig
 
 
 if __name__ == '__main__':
     app.run_server()
+
 
